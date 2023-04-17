c9c8cfc00bcc4fa48f35cb2a88a459031f7bc055
naveenmr2001 <naveenmr@localhost.localdomain>
2023-04-17 23:21:50 +0530

    Add. Added the database process of the project to footprint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,redirect,request,session,render_template,url_for
-import os,socket,multiprocessing,pickle
+import os,socket,multiprocessing,pickle,re
 import sqlite3
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
@@ -9,7 +9,8 @@ import google.auth.exceptions
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from request_file import logo,website_name
-from database import create_database
+from database import create_database,create_email_table,drop_table,create_footprint_table
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = '92736'
@@ -25,6 +26,89 @@ flow = Flow.from_client_secrets_file(
     redirect_uri='http://localhost:5000/oauth2callback'
 )
 
+def get_messages_limit(query):
+
+    try:
+
+        credentials = Credentials.from_authorized_user_info(session['credentials'])
+        service = build('gmail', 'v1', credentials=credentials)
+
+        response = service.users().messages().list(userId='me',maxResults=10).execute()
+
+        messages = []
+
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        print('Number of messages:', len(messages))
+        
+        domain_list = set()
+        missed_list = list()
+        
+        toemail = str()
+        subjectemail = str()
+
+        conn = sqlite3.connect('database.db',check_same_thread=False,timeout=5)
+        cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='emails';")
+        exists = bool(cursor.fetchone())
+
+        if(not exists):
+            create_email_table()
+
+        for message in messages:
+                msg = service.users().messages().get(userId='me', id=message['id']).execute()
+                if msg:
+                    headers = msg['payload']['headers']
+                    for header in headers:
+                        if header['name'] == 'From':
+                            sender = header['value']
+                            emails = sender.split()[-1]
+                            res = emails[emails.index('@') + 1 :]
+                            print(res)
+                            if(res[-1] == '>'):
+                                res = res[0:-1]                            
+                            domain_parts = res.split('.')
+                            res = '.'.join(domain_parts[-2:])
+                            try:
+                                socket.gethostbyname(res)
+                                domain_list.add(res)
+                            except:
+                                missed_list.append(res)
+
+                        if header['name'] == 'Subject':
+                            subjectemail = header['value']
+
+                        if header['name'] == 'To':
+                            match = re.search(r'\<(.*?)\>', header['value'])
+                            if match:
+                                toemail = match.group(1)
+                                print(toemail)
+                            else:
+                                toemail = header['value']
+                                print(toemail)
+                            
+                internal_date = int(msg['internalDate'])/1000
+                date_str = datetime.fromtimestamp(internal_date).strftime('%Y-%m-%d %H:%M:%S')
+
+                cursor.execute("SELECT * FROM emails WHERE fromemail=? AND toemail=? AND subjectemail=? AND date=?",
+                           (res, toemail, subjectemail, date_str))
+                email_id = cursor.fetchone()
+            
+                if email_id is None:
+
+                    cursor.execute("INSERT INTO emails (fromemail, toemail, subjectemail, date) VALUES (?, ?, ?, ?)",
+                                (res, toemail, subjectemail, date_str))
+                    conn.commit()
+                else:
+                    print('Email message already exists in the database.')
+
+        domain_list = list(domain_list)
+        return domain_list
+    
+    except HttpError as error:
+
+        print(f'An error occurred: {error}')
+        return []
 
 def get_messages(query):
 
@@ -33,7 +117,7 @@ def get_messages(query):
         credentials = Credentials.from_authorized_user_info(session['credentials'])
         service = build('gmail', 'v1', credentials=credentials)
 
-        response = service.users().messages().list(userId='me',maxResults=10).execute()
+        response = service.users().messages().list(userId='me',maxResults=100).execute()
 
         messages = []
 
@@ -51,6 +135,16 @@ def get_messages(query):
 
         missed_list = list()
         
+        toemail = str()
+        subjectemail = str()
+
+        conn = sqlite3.connect('database.db',check_same_thread=False)
+        cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='emails';")
+        exists = bool(cursor.fetchone())
+
+        if(not exists):
+            create_email_table()
+
         for message in messages:
                 msg = service.users().messages().get(userId='me', id=message['id']).execute()
                 if msg:
@@ -60,6 +154,7 @@ def get_messages(query):
                             sender = header['value']
                             emails = sender.split()[-1]
                             res = emails[emails.index('@') + 1 :]
+                            print(res)
                             if(res[-1] == '>'):
                                 res = res[0:-1]                            
                             domain_parts = res.split('.')
@@ -70,39 +165,137 @@ def get_messages(query):
                             except:
                                 missed_list.append(res)
 
+                        if header['name'] == 'Subject':
+                            subjectemail = header['value']
+
+                        if header['name'] == 'To':
+                            match = re.search(r'\<(.*?)\>', header['value'])
+                            if match:
+                                toemail = match.group(1)
+                                print(toemail)
+                            else:
+                                toemail = header['value']
+                                print(toemail)
+                            
+                internal_date = int(msg['internalDate'])/1000
+                date_str = datetime.fromtimestamp(internal_date).strftime('%Y-%m-%d %H:%M:%S')
+
+                cursor.execute("SELECT * FROM emails WHERE fromemail=? AND toemail=? AND subjectemail=? AND date=?",
+                           (res, toemail, subjectemail, date_str))
+                email_id = cursor.fetchone()
+            
+                if email_id is None:
+
+                    cursor.execute("INSERT INTO emails (fromemail, toemail, subjectemail, date) VALUES (?, ?, ?, ?)",
+                                (res, toemail, subjectemail, date_str))
+                    conn.commit()
+                else:
+                    print('Email message already exists in the database.')
+
         domain_list = list(domain_list)
         return domain_list
-
+    
     except HttpError as error:
 
         print(f'An error occurred: {error}')
         return []
 
-@app.route("/google")
-def call():
-    query = ''
-    pool = multiprocessing.Pool(processes=4)
-    queries = [f'{query} label:{label}' for label in ['INBOX','SENT', 'SPAM', 'TRASH']]
-    results = pool.map(get_messages,queries)
-    messages = [item for sublist in results for item in sublist]
-    value = set(messages)
-    res = list(value)
-    content_list = list()
-    for domain in res:
-        protocol = "https"
-        url = f"{protocol}://{domain}"
-        logo_data = logo(url)
-        website = website_name(url)
-        content_list.append([website,url,logo_data])
-    return render_template("footprint.html",output = content_list)
+@app.route("/footprint")
+def footprint():
+
+    userEmail = request.args.get('email')
+    print(userEmail)
+    conn = sqlite3.connect('database.db',check_same_thread=False)
+    cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='footprint';")
+    exists = bool(cursor.fetchone())
+
+    if(not exists):
+        create_footprint_table()
+
+    cursor = conn.execute("SELECT * FROM footprint WHERE email = ?", (userEmail,))
+    rows = cursor.fetchall()
+    if rows:
+        query = ''
+        pool = multiprocessing.Pool(processes=4)
+        queries = [f'{query} label:{label}' for label in ['INBOX','SENT', 'SPAM', 'TRASH']]
+        results = pool.map(get_messages_limit,queries)
+        messages = [item for sublist in results for item in sublist]
+        print(messages)
+        value = set(messages)
+        res = list(value)
+        content_list = list()
+        for domain in res:
+            protocol = "https"
+            url = f"{protocol}://{domain}"
+            logo_data = logo(url)
+            website = website_name(url)
+
+            cursor.execute("SELECT * FROM footprint WHERE name=? AND url=? AND logo=? AND email=?",
+                           (website, url, logo_data, userEmail))
+            
+            email_id = cursor.fetchone()
+
+            if email_id is None:
+                cursor.execute("INSERT INTO footprint (fromemail, toemail, subjectemail, date) VALUES (?, ?, ?, ?)",
+                        (website, url, logo_data, userEmail))
+                conn.commit()
+            else:
+                print('Email message already exists in the database.')
+
+        cursor = conn.execute("SELECT * FROM footprint WHERE email = ?", (userEmail,))
+        rows = cursor.fetchall()    
+        print(rows)
+        for row in rows:
+            name, url, logolink, email = row[0], row[1], row[2], row[3]
+            print(f"Found data for {email}: logo={logolink}, name={name}, url={url}")
+            content_list.append([name,url,logolink])
+
+        return render_template("footprint.html",output = content_list)
+    else:
+        query = ''
+        pool = multiprocessing.Pool(processes=4)
+        queries = [f'{query} label:{label}' for label in ['INBOX','SENT', 'SPAM', 'TRASH']]
+        results = pool.map(get_messages,queries)
+        messages = [item for sublist in results for item in sublist]
+        value = set(messages)
+        res = list(value)
+        content_list = list()
+        for domain in res:
+            protocol = "https"
+            url = f"{protocol}://{domain}"
+            logo_data = logo(url)
+            website = website_name(url)
+
+            cursor.execute("SELECT * FROM footprint WHERE name=? AND url=? AND logo=? AND email=?",
+                        (website, url, logo_data, userEmail))
+        
+            email_id = cursor.fetchone()
+
+            if email_id is None:
+                cursor.execute("INSERT INTO footprint (name, url, logo, email) VALUES (?, ?, ?, ?)",
+                        (website, url, logo_data, userEmail))
+                conn.commit()
+            else:
+                print('Email message already exists in the database.')
+
+        cursor = conn.execute("SELECT * FROM footprint WHERE email = ?", (userEmail,))
+        rows = cursor.fetchall()    
+        print(rows)
+        for row in rows:
+            name, url, logolink, email = row[0], row[1], row[2], row[3]
+            print(f"Found data for {email}: logo={logolink}, name={name}, url={url}")
+            content_list.append([name,url,logolink])
+        return render_template("footprint.html",output = content_list)
+    
 
 
 
 @app.route("/main")
 def main():
+    email = request.args.get('email')
     if 'credentials' not in session:
         return redirect('/')
-    return render_template("main.html")
+    return render_template("main.html",email=email)
 
 @app.route('/signup',methods=["POST"])
 def signup():
@@ -118,7 +311,7 @@ def signup():
         query = "INSERT INTO users (name, gmail, logo, password) VALUES (?, ?, ?, ?);"
         conn.execute(query, (name, email, photo, password))
         conn.commit()
-        return url_for('main',email=email)
+        return url_for('signin',email=email)
 
 @app.route('/signin',methods=['POST','GET'])
 def signin():
@@ -136,15 +329,15 @@ def signin():
         user = cursor.fetchone()
         conn.close()
         if(user):
-            return redirect("/main")
+            return redirect(url_for('main',email=email))
         else:
-            return redirect("/")
+            return redirect(url_for('/'))
 
 @app.route('/displayUserDetails',methods=['GET', 'POST'])
 def displayUserDetails():
 
     if 'credentials' not in session:
-        return redirect('/')
+        return redirect(url_for('/'))
 
     name = request.args.get('name')
 
@@ -159,7 +352,7 @@ def displayUserDetails():
 @app.route('/userDetails')
 def userDetails():
     if 'credentials' not in session:
-        return redirect('/')
+        return redirect(url_for('/'))
 
     user_present = False
 
@@ -187,7 +380,7 @@ def userDetails():
     if 'names' in photo_response:
         user_name = photo_response['names'][0]['displayName']
 
-    conn = sqlite3.connect('database.db',check_same_thread=False,timeout=5)
+    conn = sqlite3.connect('database.db',check_same_thread=False)
     cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users';")
     exists = bool(cursor.fetchone())
 
@@ -238,12 +431,11 @@ def login():
 @app.route("/sign")
 def sign():
     if 'credentials' not in session:
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 @app.route('/')
 
 def index():
-
     return render_template("login.html")
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -10,13 +10,33 @@ def create_database():
     conn.commit()
     conn.close()
 
-# def drop_table():
-#     conn = sqlite3.connect('database.db')
-#     cursor = conn.cursor()
+def create_email_table():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE emails(fromemail TEXT, toemail TEXT, subjectemail TEXT,date TEXT);")
+    conn.commit()
+    conn.close()
+
+def create_footprint_table():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE footprint
+                 (name TEXT,
+                  url TEXT,
+                  logo TEXT,
+                  email TEXT)''')
+    conn.commit()
+    conn.close()
+
+def drop_table():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
     
-#     cursor.execute('''DROP TABLE users''');
-#     conn.commit()
-#     conn.close()
+    cursor.execute('''DROP TABLE emails''');
+    conn.commit()
+    conn.close()
 
 # if __name__ == "__main__":
 #     drop_table()
